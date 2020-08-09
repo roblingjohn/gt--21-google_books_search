0188f9fe84e812658a873a4eadd0879dab9448a9
John Robling <roblingjohn@gmail.com>
2020-08-09 15:58:01 -0400

    connected front end and back end servers

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,13 @@
-import React from 'react';
 import './App.css';
+import React, {useEffect} from 'react';
+import axios from 'axios';
 
 function App() {
+  useEffect(() => {
+    axios.get("/api/config").then((res) => {
+      console.log(res.data);
+    });
+  });
   return (
     <div className="App">
       <h1>Hello world</h1>
