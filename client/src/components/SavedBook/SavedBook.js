549c828bd92cec81f7065309a67c18a8b75ff484
John Robling <roblingjohn@gmail.com>
2020-08-10 19:34:41 -0400

    added view feature to saved book

--- client/src/components/SavedBook/SavedBook.js
+++ client/src/components/SavedBook/SavedBook.js
@@ -1,13 +1,17 @@
 import React from "react";
 
 const SavedBook = (props) => {
+  const handleView = (event) => {
+    event.preventDefault();
+    window.location.assign(props.link);
+  };
   return (
     <div className="container">
       <h3>{props.title}</h3>
       <h4>{props.author}</h4>
       <img src={props.image} alt={props.title} />
       <p>{props.description}</p>
-      <button className="view">View</button>
+      <button className="view" onClick={handleView}>View</button>
       <button className="save">Delete</button>
     </div>
   );
